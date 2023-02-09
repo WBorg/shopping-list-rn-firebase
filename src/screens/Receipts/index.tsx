31d74539aaf0085c233bc663f3819504d5e1b4b1
WILLY BORGES SILVA <willy.bsilva@senacsp.edu.br>
2023-02-08 21:29:17 -0300

    crud completo

--- src/screens/Receipts/index.tsx
+++ src/screens/Receipts/index.tsx
@@ -1,37 +1,67 @@
-import React, {useState, useEffect} from 'react';
-import { FlatList } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react'
+import { FlatList, Alert } from 'react-native'
 import storage from '@react-native-firebase/storage'
-import { Container, PhotoInfo } from './styles';
-import { Header } from '../../components/Header';
-import { Photo } from '../../components/Photo';
-import { File, FileProps } from '../../components/File';
+import { Container, PhotoInfo } from './styles'
+import { Header } from '../../components/Header'
+import { Photo } from '../../components/Photo'
+import { File, FileProps } from '../../components/File'
+import { useFocusEffect } from '@react-navigation/native'
 
 // import { photosData } from '../../utils/photo.data';
 
 export function Receipts() {
-
-  const [photos ,  setPhotos] = useState<FileProps[]>([])
+  const [photos, setPhotos] = useState<FileProps[]>([])
   const [photoSelected, setPhotSelected] = useState('')
+  const [photoInfo, setPhotoInfo] = useState('')
 
-  async function handleShowImage(path: string){
+  async function handleShowImage(path: string) {
     const urlImage = await storage().ref(path).getDownloadURL()
     setPhotSelected(urlImage)
+    // trazendo informações do arquivo
+    const info = await storage().ref(path).getMetadata()
+    setPhotoInfo(
+      `Upload realizado em  ${info.timeCreated}  \nTamanho: ${info.size} bytes`
+    )
+  }
+  async function handlDeleteImage(path: string) {
+    storage()
+      .ref(path)
+      .delete()
+      .then(() => {
+        Alert.alert('Imagem excluída copm sucesso!!')
+        fetchImages()
+        setPhotSelected('')
+        setPhotoInfo('')
+      })
+      .catch(error => console.log(error))
+  }
+  async function fetchImages() {
+    storage()
+      .ref('images')
+      .list()
+      .then(result => {
+        const files: FileProps[] = []
+
+        result.items.forEach(file => {
+          files.push({
+            name: file.name,
+            path: file.fullPath
+          })
+
+          setPhotos(files)
+        })
+      })
   }
 
-  useEffect(()=>{
-    storage().ref('images').list().then( result => {
-      const files: FileProps[] = []
+  useEffect(() => {
+    fetchImages()
+  }, [])
 
-      result.items.forEach( file =>{
-        files.push({
-          name: file.name,
-          path: file.fullPath
-        })
-
-        setPhotos(files)
-      })
-    })
-  },[])
+  useFocusEffect(
+    useCallback(() => {
+      fetchImages()
+    }, [])
+  )
 
   return (
     <Container>
@@ -39,9 +69,7 @@ export function Receipts() {
 
       <Photo uri={photoSelected} />
 
-      <PhotoInfo>
-        Informações da foto
-      </PhotoInfo>
+      <PhotoInfo>{photoInfo}</PhotoInfo>
 
       <FlatList
         data={photos}
@@ -50,7 +78,7 @@ export function Receipts() {
           <File
             data={item}
             onShow={() => handleShowImage(item.path)}
-            onDelete={() => { }}
+            onDelete={() => handlDeleteImage(item.path)}
           />
         )}
         contentContainerStyle={{ paddingBottom: 100 }}
@@ -58,5 +86,5 @@ export function Receipts() {
         style={{ width: '100%', padding: 24 }}
       />
     </Container>
-  );
+  )
 }
